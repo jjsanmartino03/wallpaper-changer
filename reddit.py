f3de861102619d8d6dce1df851b5702625705185
Julián Sanmartino <jjsanmartino03@gmail.com>
2020-10-05 12:58:59 -0300

    Finish reddit.py

--- reddit.py
+++ reddit.py
@@ -1,11 +1,18 @@
 import os
-import praw
+import praw # Python Reddit API Wrapper (PRAW). More information at "https://praw.readthedocs.io/en/latest/index.html"
 from prawcore.exceptions import RequestException
 from dotenv import load_dotenv
 
-load_dotenv() # Add the varibles in the .env file to the global environment
+"""
+To access the Reddit API you must have a client_id, a client_secret,and a user_agent (user_agent can be any string of text). You can easily obtain those by creating your own app at 'https://old.reddit.com/prefs/apps/'. If you don't have a redirect uri (obligatory to create the app), you can use instead 'http://www.example.com/unused/redirect/uri', to fill that field.
+
+For this program to work, you must add the id and secret to the .env file in this directory in the format:
+CLIENT_SECRET="xxxxx"
+CLIENT_ID="yyyyy"
+"""
 
 def get_image_url():
+    load_dotenv() # Add the varibles in the .env file to the global environment
 
     r = praw.Reddit(
         client_id=os.getenv("CLIENT_ID"), # Get the environmental variables
