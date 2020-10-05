7ae2e8a08050db2689ba842b1fa65321d69c4c50
Julián Sanmartino <jjsanmartino03@gmail.com>
2020-10-05 10:56:36 -0300

    Redid way to get image url

--- reddit.py
+++ reddit.py
@@ -1,34 +1,28 @@
 import os
-import random
 import praw
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def get_image_url():
+
     r = praw.Reddit(
         client_id=os.getenv("CLIENT_ID"),
         client_secret=os.getenv("CLIENT_SECRET"),
         user_agent="Wallpaper getter by u/Chu-lian13", #This is an example of user_agent, you can put whatever you want
         )
-    
-        
-        
 
-    actual_sub = random.choice(("wallpapers", "wallpaper"))
-    post_number = random.randint(0, 998)
-    count = 0
     try:
-        for i in r.subreddit(actual_sub).top("all", limit=999):
-            if count == post_number:
-                if ".jpg" in i.url:
-                    return i.url
-                else:
-                    return get_image_url()
-            count += 1
+        image_url = r.subreddit("wallpaper").random().url
+
+        while ".jpg" != image_url[-4:]:
+            image_url = image_url = r.subreddit("wallpaper").random().url
+    
+        return image_url
+
     except RequestException:
         return False
 
 
 if __name__ == "__main__":
-    get_image_url()
+    print(get_image_url())
