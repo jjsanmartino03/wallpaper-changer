b69e7b7f56f8b1e79249cb93e65c2f788ae4b5b5
Julián Sanmartino <jjsanmartino03@gmail.com>
2021-08-02 07:53:12 -0300

    add bash script for quotes

---
+++ quotes.sh
@@ -0,0 +1,8 @@
+# In this file you can put the arguments you want to pass to the app,
+# and execute it from any directory of your pc
+
+# the absolute path to your python interpreter if using a virtual environment,
+# or simply "python3" if you don't
+# for example: /home/user/wall-changer-venv/bin/python3 ~/Desktop/wallpaper-changer/main.py
+/home/julian/projects/wallpaper-changer/wall-changer-venv/bin/python3 /home/julian/projects/wallpaper-changer/quotes-scrapper.py
+# Notice that the path must not have a space in the middle, or will be treated as different strings
